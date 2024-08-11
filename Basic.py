e2ddb7d45f1542e9db12e59b5f0d51aec13fd7ee
DattaPraveen06 <praveendattaviriventi@gmail.com>
2024-08-11 11:20:39 +0530

    This are basic python programs

---
+++ Basic.py
@@ -0,0 +1,13 @@
+#1) You need to print a string in the following format ..
+    # The Order is:
+    # Apples : 30
+    # Oranges : 10
+    # Total : 40 Units
+
+Apples = 30
+Oranges = 10
+c = Apples + Oranges
+print(f'Apples : {Apples}', f'Oranges : {Oranges}', f'Total :{c} Units')
+#-------------------------------------------------------------------------------
+
+# 2)I have a string "hello world" I need the following output "world hello"
